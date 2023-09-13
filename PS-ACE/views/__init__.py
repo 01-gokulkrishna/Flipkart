d7906358d1d656080ab193232c1f9d51bd2b5b96
01-gokulkrishna <55729441+01-gokulkrishna@users.noreply.github.com>
2023-09-13 09:53:04 +0530

    Delete PS-ACE directory

--- PS-ACE/views/__init__.py
+++
@@ -1,9 +0,0 @@
-from views.base_view import BaseView
-from views.availability_view import AvailabilityView
-from views.exchange_view import ExchangeView
-from views.search_view import SearchView
-
-'''from views.search_view import SearchView,SearchViewIOS
-from views.availability_view import PostsView,PostsViewIOS
-from views.profile_view import ProfileView,ProfileViewIOS
-from views.video_view import VideoView,VideoViewIOS'''
